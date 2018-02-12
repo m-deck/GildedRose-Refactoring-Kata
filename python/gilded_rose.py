67b08ecc23fae2fcbdb4c6623af9e7b7da655a7a
Mike Deck <mike.deck@avant.com>
2018-02-11 23:02:33 -0600

    cap initial quality of item to 50

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -40,7 +40,7 @@ class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
-        self.quality = quality
+        self.quality = min(50, quality)
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
