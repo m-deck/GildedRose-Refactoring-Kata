366c6e938ed28e7dc0e1ae44a555c9ffc688c6a2
Mike Deck <mike.deck@avant.com>
2018-08-08 18:56:54 -0500

    revert change to the Item class. sorry to the coding gods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -40,7 +41,7 @@ class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
-        self.quality = min(50, quality)
+        self.quality = quality
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
